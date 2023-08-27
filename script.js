18857210f89e57742662bef7fb0de01d1fd3aaa4
Dong Van Dat <dongvandat9054@gmail.com>
2023-08-27 23:43:04 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let blackBtn=document.getElementById("black");
+let yellowBtn=document.getElementById("yellow");
+let whiteBtn=document.getElementById("white");
+let bike=document.getElementById("bike");
+
+blackBtn.onclick=function(){
+    bike.style.backgroundImage="url(/img/carden.png)";
+}
+
+yellowBtn.onclick=function(){
+    bike.style.backgroundImage="url(/img/carvang.png)";
+}
+
+whiteBtn.onclick=function(){
+    bike.style.backgroundImage="url(/img/Untitled-1.png)";
+}
+
